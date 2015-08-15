edd6374ebd1aab95a7e62eddd5d02135761a1201
abc <abc@abcs-MacBook-Pro.local>
2015-08-15 12:06:12 +0530

    first output, failed correlation test

--- main.py
+++ main.py
@@ -29,18 +29,21 @@ while (i < ( len(train[0] ) )):
     if (train[0][i] == 'SPDhits'):
         da.delCollumn(train,i)
 
-#get the signal collumn to predict
+    #get the signal collumn to predict
     if ( train[0][i] == 'signal'):
         signal = da.getCollumn(train,i)
         da.delCollumn(train,i)
         i = i - 4
     i = i + 1
-#remove the header
-
+signal = [int(i) for  i in signal[1:]]
+train  = [[float(i) for i in y ] y in train[1:]]
+for i in signal[1:]:
+    if ( i != 1 and  i != 0 ):
+        print i
 #now train
-from sklearn.ensemble import RandomForestRegressor
-clf = RandomForestRegressor(random_state = 1, n_estimators=100)
-clf.fit(train[1:],signal[1:])
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(random_state = 1, n_estimators=10)
+clf.fit(train[1:],signal)
 
 #now predict
 test = csv.reader( open(r'test.csv'))
@@ -51,11 +54,36 @@ for i in range(len(test[0])):
 
     if ( test[0][i] == 'SPDhits'):
         da.delCollumn(test,i)
-print len(test[1])
-print len(train[1])
 for i in test[1:]:
+    b = clf.predict_proba(i)
+    c.writerow( [i[0] , (b[:,1][0])]  )
 
-    c.writerow( [i[0] , str( clf.predict_proba(i) ) ] )
+# Validate
+from sklearn import metrics
+
+p = []
+for i in train[1:]:
+    b = clf.predict_proba(i)
+    p.append(b[:,1][0])
+
+
+fpr, tpr, thresholds = metrics.roc_curve(signal,p )
+score = 0
+for i in tpr :
+    if ( i <= 0.2 ) :
+        score += i * 0.2
+    if ( i>0.2 and i <= 0.4):
+        score += i * 1.5
+    if ( i > 0.4 and i <= 0.6):
+        score += i * 1.0
+    if ( i> 0.6 and i <= 0.8):
+        score += i * 0.5
+    if ( i > 0.8):
+        score += i * 0.0
+
+print score
+print "complete"
+
 
 
 
